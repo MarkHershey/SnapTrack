03d2c0ccfe0aa08dc22f3fc58ea8cbbd0e59c3d8
miyucloud <ongzhiyi@live.com>
2020-12-02 14:51:45 +0800

    data analytics work in progress

--- SnapTrackApp/app/src/main/java/com/example/snaptrackapp/ui/analytics/AnalyticsFragment.java
+++ SnapTrackApp/app/src/main/java/com/example/snaptrackapp/ui/analytics/AnalyticsFragment.java
@@ -9,16 +9,33 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.snaptrackapp.R;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AnalyticsFragment extends Fragment {
 
     private AnalyticsViewModel analyticsViewModel;
+
+    BarChart barChart;
+    BarData barData;
+    ArrayList<BarEntry> barEntries = new ArrayList<>();
+    private Date startDate;
+    private Date endDate;
+    private Calendar c = Calendar.getInstance();
 
     public static AnalyticsFragment newInstance() {
         return new AnalyticsFragment();
@@ -27,7 +44,8 @@ public class AnalyticsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_analytics, container, false);
+        View root = inflater.inflate(R.layout.daily_fragment, container, false);
+        barChart = root.findViewById(R.id.daily_barChart);
         return root;
     }
 
@@ -35,14 +53,32 @@ public class AnalyticsFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         analyticsViewModel = new ViewModelProvider(this).get(AnalyticsViewModel.class);
-        // TODO: Use the ViewModel
-        /*analyticsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>(){
 
-            @Override
-            public void onChanged(String s) {
-                text_analytics.setText(s);
-            }
-        });*/
+
+        Log.v("logcaat","start daily fragment");
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        startDate = c.getTime();
+
+        c.set(Calendar.HOUR_OF_DAY, 23);
+        c.set(Calendar.MINUTE, 59);
+        c.set(Calendar.SECOND, 59);
+        endDate = c.getTime();
+
+//        getData();
+        barEntries.add(new BarEntry(4, new float[]{2, 5.5f, 4}));
+        barEntries.add(new BarEntry(5, new float[]{3, 4.3f, 4}));
+        barEntries.add(new BarEntry(6, new float[]{4, 2.3f, 4}));
+        barEntries.add(new BarEntry(7, new float[]{1, 5.6f, 4}));
+        barEntries.add(new BarEntry(8, new float[]{2, 2.4f, 4}));
+        Log.v("logcaat",barEntries.toString());
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "time");
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+
+        barData = new BarData(barDataSet);
+
+        barChart.setData(barData);
+
     }
 
 }
